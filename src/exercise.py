4f19ab1bbbe91ea7e90a53c304b12f61b6056561
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 13:45:56 +0000

    exercise

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+  print_until_number(5)
 
+def print_until_number(number):
+  for i in range(1,number+1):
+    print(i)
+    
 if __name__ == '__main__':
     main()
